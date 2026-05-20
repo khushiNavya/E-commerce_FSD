74ce20d50280c1cab073d594b2002684020de648
khushiNavya <khushisinghrana111@gmail.com>
2026-05-20 23:59:02 +0530

    Checked

--- E-commerece website/src/Constants.js
+++ E-commerece website/src/Constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://e-commerce-fsd.onrender.com";
+export const baseUrl = "https://e-commerce-fsd.onrender.com/";
 export const signupUrl = `${baseUrl}users/signup`;
 export const loginUrl = `${baseUrl}users/login`;
-export const getUser = `${baseUrl}users`
+export const getUser = `${baseUrl}users`;
